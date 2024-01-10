2bb969c622d438ffd2caf814aa742107390fd51c
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-01-10 23:16:48 +0200

    Create magicMatrices.js

---
+++ Arrays and Nested Arrays - Exercise/magicMatrices.js
@@ -0,0 +1,19 @@
+function magicMatrices(array) {
+
+    let sumFirstLine = array[0].reduce((a, b) => a + b);
+    for (let i = 0; i < array[0].length; i++) {
+        let currentColums = 0;
+        for (let j = 0; j < array.length; j++) {
+            currentColums += array[j][i]
+        }
+        if (currentColums !== sumFirstLine) {
+            return false;
+        }
+    }
+    return true;
+
+}
+magicMatrices([[4, 5, 6],
+[6, 5, 4],
+[5, 5, 5]]
+)
