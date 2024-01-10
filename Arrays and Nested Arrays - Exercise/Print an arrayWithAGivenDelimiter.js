709696e3368531985bf4f69d96167e789de466b4
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-01-10 23:16:51 +0200

    Create Print an arrayWithAGivenDelimiter.js

---
+++ Arrays and Nested Arrays - Exercise/Print an arrayWithAGivenDelimiter.js
@@ -0,0 +1,12 @@
+function arrayWithAGivenDelimiter(arr, delimiter) {
+
+    let result=arr.join(delimiter);
+    console.log(result);
+
+}
+arrayWithAGivenDelimiter(['One',
+    'Two',
+    'Three',
+    'Four',
+    'Five'],
+    '-');
