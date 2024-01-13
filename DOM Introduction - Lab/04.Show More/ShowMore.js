b09fb1c019fef6b79fb6760432da05fcf6ad6966
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-01-13 22:47:46 +0200

    Create ShowMore.js

---
+++ DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -0,0 +1,11 @@
+function showText() {
+
+    let span = document.getElementById("text");
+    let anchor = document.getElementById("more");
+
+    span.style.display = "inline";
+    anchor.style.display = "none";
+}
+
+
+
